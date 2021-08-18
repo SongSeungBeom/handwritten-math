4159061ae6c78dd4817d1d82fc4c2f41bb490e04
SongSeungBeom <radiopc3@gmail.com>
2021-08-18 21:50:38 +0900

    second commit

---
+++ simple_math_model.py
@@ -0,0 +1,92 @@
+from PIL import Image
+import matplotlib
+import matplotlib.pyplot as plt
+import torch
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import torchvision
+from torchvision import transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+trans = transforms.Compose([transforms.Resize((32, 32)), transforms.ToTensor(), transforms.Normalize((0.5,0.5, 0.5),(0.5,0.5,0.5))])
+
+trainset = torchvision.datasets.ImageFolder(root = "./loaderdata/train", transform=trans)
+testset =  torchvision.datasets.ImageFolder(root = "./loaderdata/test", transform=trans)
+
+trainloader = DataLoader(trainset, batch_size=4, shuffle=True, num_workers=4)
+testloader = DataLoader(testset, batch_size=4, shuffle=True, num_workers=4)
+
+classes = trainset.classes
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 49)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # 배치를 제외한 모든 차원을 평탄화(flatten)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+if __name__ == '__main__':
+    net = Net()
+    net.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    for epoch in range(15):  # 데이터셋을 수차례 반복합니다.
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # [inputs, labels]의 목록인 data로부터 입력을 받은 후;
+            inputs, labels = data[0].to(device), data[1].to(device)
+
+            # 변화도(Gradient) 매개변수를 0으로 만들고
+            optimizer.zero_grad()
+
+            # 순전파 + 역전파 + 최적화를 한 후
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # 통계를 출력합니다.
+            running_loss += loss.item()
+            if i % 2000 == 1999:  # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+    print('Finished Training')
+
+    PATH = './model/math_net_02.pth'
+    torch.save(net.state_dict(), PATH)
+
+    correct = 0
+    total = 0
+    # 학습 중이 아니므로, 출력에 대한 변화도를 계산할 필요가 없습니다
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data[0].to(device), data[1].to(device)
+            # 신경망에 이미지를 통과시켜 출력을 계산합니다
+            outputs = net(images)
+            # 가장 높은 값(energy)를 갖는 분류(class)를 정답으로 선택하겠습니다
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the 180000 test images: %d %%' % (100 * correct / total))
