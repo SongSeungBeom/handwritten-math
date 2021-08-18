b351b2001bfa2f38e582991d01dc362558a0fb27
SongSeungBeom <radiopc3@gmail.com>
2021-08-18 21:51:50 +0900

    third commit

---
+++ math_densenet.py
@@ -0,0 +1,275 @@
+from PIL import Image
+import matplotlib
+import matplotlib.pyplot as plt
+import torch
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import torchvision
+from torchvision import transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import math
+import time
+import copy
+import os
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+train_trans = transforms.Compose([transforms.Resize((64, 64)), transforms.ToTensor(), transforms.Normalize((0.5,0.5, 0.5),(0.5,0.5,0.5))])
+
+test_trans = transforms.Compose([transforms.Resize((64, 64)), transforms.ToTensor(), transforms.Normalize((0.5,0.5, 0.5),(0.5,0.5,0.5))])
+
+trainset = torchvision.datasets.ImageFolder(root = "./loaderdata/train", transform=train_trans)
+testset =  torchvision.datasets.ImageFolder(root = "./loaderdata/test", transform=test_trans)
+
+train_dl = DataLoader(trainset, batch_size=32, shuffle=True, num_workers=4)
+val_dl = DataLoader(testset, batch_size=32, shuffle=True, num_workers=4)
+
+classes = trainset.classes
+
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+class BottleNeck(nn.Module):
+    def __init__(self, in_channels, growth_rate):
+        super().__init__()
+        inner_channels = 4 * growth_rate
+
+        self.residual = nn.Sequential(
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU(),
+            nn.Conv2d(in_channels, inner_channels, 1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(inner_channels),
+            nn.ReLU(),
+            nn.Conv2d(inner_channels, growth_rate, 3, stride=1, padding=1, bias=False)
+        )
+
+        self.shortcut = nn.Sequential()
+
+    def forward(self, x):
+        return torch.cat([self.shortcut(x), self.residual(x)], 1)
+
+class Transition(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+
+        self.down_sample = nn.Sequential(
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU(),
+            nn.Conv2d(in_channels, out_channels, 1, stride=1, padding=0, bias=False),
+            nn.AvgPool2d(2, stride=2)
+        )
+
+    def forward(self, x):
+        return self.down_sample(x)
+
+
+class DenseNet(nn.Module):
+    def __init__(self, nblocks, growth_rate=12, reduction=0.5, num_classes=49, init_weights=True):
+        super().__init__()
+
+        self.growth_rate = growth_rate
+        inner_channels = 2 * growth_rate  # output channels of conv1 before entering Dense Block
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, inner_channels, 7, stride=2, padding=3),
+            nn.MaxPool2d(3, 2, padding=1)
+        )
+
+        self.features = nn.Sequential()
+
+        for i in range(len(nblocks) - 1):
+            self.features.add_module('dense_block_{}'.format(i), self._make_dense_block(nblocks[i], inner_channels))
+            inner_channels += growth_rate * nblocks[i]
+            out_channels = int(reduction * inner_channels)
+            self.features.add_module('transition_layer_{}'.format(i), Transition(inner_channels, out_channels))
+            inner_channels = out_channels
+
+        self.features.add_module('dense_block_{}'.format(len(nblocks) - 1),
+                                 self._make_dense_block(nblocks[len(nblocks) - 1], inner_channels))
+        inner_channels += growth_rate * nblocks[len(nblocks) - 1]
+        self.features.add_module('bn', nn.BatchNorm2d(inner_channels))
+        self.features.add_module('relu', nn.ReLU())
+
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.linear = nn.Linear(inner_channels, num_classes)
+
+        # weight initialization
+        if init_weights:
+            self._initialize_weights()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.features(x)
+        x = self.avg_pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.linear(x)
+        return x
+
+    def _make_dense_block(self, nblock, inner_channels):
+        dense_block = nn.Sequential()
+        for i in range(nblock):
+            dense_block.add_module('bottle_neck_layer_{}'.format(i), BottleNeck(inner_channels, self.growth_rate))
+            inner_channels += self.growth_rate
+        return dense_block
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+
+def DenseNet_121():
+    return DenseNet([6, 12, 24, 6])
+
+
+def get_lr(opt):
+    for param_group in opt.param_groups:
+        return param_group['lr']
+
+
+# calculate the metric per mini-batch
+def metric_batch(output, target):
+    pred = output.argmax(1, keepdim=True)
+    corrects = pred.eq(target.view_as(pred)).sum().item()
+    return corrects
+
+
+# calculate the loss per mini-batch
+def loss_batch(loss_func, output, target, opt=None):
+    loss_b = loss_func(output, target)
+    metric_b = metric_batch(output, target)
+
+    if opt is not None:
+        opt.zero_grad()
+        loss_b.backward()
+        opt.step()
+
+    return loss_b.item(), metric_b
+
+
+# calculate the loss per epochs
+def loss_epoch(model, loss_func, dataset_dl, sanity_check=False, opt=None):
+    running_loss = 0.0
+    running_metric = 0.0
+    len_data = len(dataset_dl.dataset)
+
+    for xb, yb in dataset_dl:
+        xb = xb.to(device)
+        yb = yb.to(device)
+        output = model(xb)
+
+        loss_b, metric_b = loss_batch(loss_func, output, yb, opt)
+
+        running_loss += loss_b
+
+        if metric_b is not None:
+            running_metric += metric_b
+
+        if sanity_check is True:
+            break
+
+    loss = running_loss / len_data
+    metric = running_metric / len_data
+    return loss, metric
+
+
+# function to start training
+def train_val(model, params):
+    num_epochs = params['num_epochs']
+    loss_func = params['loss_func']
+    opt = params['optimizer']
+    train_dl = params['train_dl']
+    val_dl = params['val_dl']
+    sanity_check = params['sanity_check']
+    lr_scheduler = params['lr_scheduler']
+    path2weights = params['path2weights']
+
+    loss_history = {'train': [], 'val': []}
+    metric_history = {'train': [], 'val': []}
+
+    best_loss = float('inf')
+    best_model_wts = copy.deepcopy(model.state_dict())
+    start_time = time.time()
+
+    for epoch in range(num_epochs):
+        current_lr = get_lr(opt)
+        print('Epoch {}/{}, current lr= {}'.format(epoch, num_epochs - 1, current_lr))
+
+        model.train()
+        train_loss, train_metric = loss_epoch(model, loss_func, train_dl, sanity_check, opt)
+        loss_history['train'].append(train_loss)
+        metric_history['train'].append(train_metric)
+
+        model.eval()
+        with torch.no_grad():
+            val_loss, val_metric = loss_epoch(model, loss_func, val_dl, sanity_check)
+        loss_history['val'].append(val_loss)
+        metric_history['val'].append(val_metric)
+
+        if val_loss < best_loss:
+            best_loss = val_loss
+            best_model_wts = copy.deepcopy(model.state_dict())
+            torch.save(model.state_dict(), path2weights)
+            print('Copied best model weights!')
+
+        lr_scheduler.step(val_loss)
+        if current_lr != get_lr(opt):
+            print('Loading best model weights!')
+            model.load_state_dict(best_model_wts)
+
+        print('train loss: %.6f, val loss: %.6f, accuracy: %.2f, time: %.4f min' % (
+        train_loss, val_loss, 100 * val_metric, (time.time() - start_time) / 60))
+        print('-' * 10)
+
+    model.load_state_dict(best_model_wts)
+    return model, loss_history, metric_history
+
+# check the directory to save weights.pt
+if __name__ == '__main__':
+    x = torch.randn(3, 3, 64, 64)
+    model = DenseNet_121()
+    output = model(x)
+    print(output.size())
+    loss_func = nn.CrossEntropyLoss(reduction='sum')
+    opt = optim.Adam(model.parameters(), lr=0.01)
+    lr_scheduler = ReduceLROnPlateau(opt, mode='min', factor=0.1, patience=8)
+    params_train = {
+        'num_epochs': 30,
+        'optimizer': opt,
+        'loss_func': loss_func,
+        'train_dl': train_dl,
+        'val_dl': val_dl,
+        'sanity_check': False,
+        'lr_scheduler': lr_scheduler,
+        'path2weights': './model/weights.pt',
+    }
+    model, loss_hist, metric_hist = train_val(model, params_train)
+    num_epochs = params_train['num_epochs']
+
+    # plot loss progress
+    plt.title('Train-Val Loss')
+    plt.plot(range(1, num_epochs + 1), loss_hist['train'], label='train')
+    plt.plot(range(1, num_epochs + 1), loss_hist['val'], label='val')
+    plt.ylabel('Loss')
+    plt.xlabel('Training Epochs')
+    plt.legend()
+    plt.show()
+
+    # plot accuracy progress
+    plt.title('Train-Val Accuracy')
+    plt.plot(range(1, num_epochs + 1), metric_hist['train'], label='train')
+    plt.plot(range(1, num_epochs + 1), metric_hist['val'], label='val')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Training Epochs')
+    plt.legend()
+    plt.show()
